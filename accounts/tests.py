5b73a8d83ff58c7ac91c7b09e4dac2ededcc912a
Your Name <you@example.com>
2026-02-17 10:52:02 +0530

    Update workflows to Python 3.12, unpin dependencies, and add basic test case

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import UserCredential
 
-# Create your tests here.
+class UserCredentialTests(APITestCase):
+    def test_hello_world(self):
+        """
+        Ensure hello_world endpoint returns 200 OK.
+        """
+        url = reverse('hello_world')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {"message": "HELLO WORLD"})
